668a8a8bc7678f0112f310a9125eb181d1de70d2
Laurent Georget <laurent@lgeorget.eu>
2016-10-02 15:08:33 +0200

    Add the support for command-line options

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <syslog.h>
 #include <cerrno>
@@ -6,32 +7,64 @@
 #include <cstdlib>
 
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 
 #include <tuple>
 
+#include "config.h"
 #include "dbconnection.h"
 #include "meteo_server.h"
 
-using namespace meteodata;
+#define DEFAULT_CONFIG_FILE "/etc/meteodata/db_credentials"
 
-static void checkUsage(int argc, char** argv)
-{
-	if (argc > 1) {
-		std::cerr << "Usage: " << argv[0] << std::endl;
-		std::exit(1);
-	}
-}
+using namespace meteodata;
+namespace po = boost::program_options;
+
 int main(int argc, char** argv)
 {
-	checkUsage(argc,argv);
-
 	std::string user;
 	std::string password;
-	std::cout << "Please give the username and password to access the database.\n";
-	std::cout << "Username: ";
-	std::cin >> user;
-	std::cout << "Password: ";
-	std::cin >> password;
+
+	po::options_description config("Configuration");
+	config.add_options()
+		("user,u", po::value<std::string>(&user), "database username")
+		("password,p", po::value<std::string>(&password), "database password")
+	;
+
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "display the help message and exit")
+		("version", "display the version of Meteodata and exit")
+		("config-file", po::value<std::string>(), "alternative configuration file")
+	;
+	desc.add(config);
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	std::string configFileName = vm.count("config-file") ?
+		vm["config-file"].as<std::string>() :
+		DEFAULT_CONFIG_FILE;
+	std::ifstream configFile(DEFAULT_CONFIG_FILE);
+	if (configFile) {
+		po::store(po::parse_config_file(configFile, config), vm);
+		configFile.close();
+	}
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << PACKAGE_STRING"\n";
+		std::cout << "Usage: " << argv[0] << " [-u user -p password]\n";
+		std::cout << desc << "\n";
+		std::cout << "You must give either both the username and "
+			"password or none of them." << std::endl;
+
+		return 0;
+	}
+
+	if (vm.count("version")) {
+		std::cout << VERSION << std::endl;
+		return 0;
+	}
 
 	int daemonization = daemon(0,0);
 
