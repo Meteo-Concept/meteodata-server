607a3dcb0fa37eea629c84c71ee034bc7a2c2e56
Laurent Georget <laurent.georget@meteo-concept.fr>
2018-08-27 11:43:36 +0200

    Make a standalone synop parser

---
+++ src/synop_standalone.h
@@ -0,0 +1,58 @@
+/**
+ * @file synop_standalone.h
+ * @brief Definition of the SynopStandalone class
+ * @author Laurent Georget
+ * @date 2018-08-28
+ */
+/*
+ * Copyright (C) 2018  SAS Météo Concept <contact@meteo-concept.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SYNOP_STANDALONE_H
+#define SYNOP_STANDALONE_H
+
+#include <iostream>
+#include <string>
+
+#include <cassandra.h>
+#include <date/date.h>
+
+#include "dbconnection.h"
+
+
+namespace meteodata {
+
+using namespace meteodata;
+
+/**
+ */
+class SynopStandalone
+{
+public:
+	SynopStandalone(DbConnection& db);
+	void start(const std::string& file);
+
+private:
+	DbConnection& _db;
+	std::map<std::string, CassUuid> _icaos;
+
+	static constexpr char HOST[] = "www.ogimet.com";
+	static constexpr char GROUP_FR[] = "07";
+};
+
+}
+
+#endif
