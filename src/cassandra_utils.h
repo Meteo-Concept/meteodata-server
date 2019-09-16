381a6e9d276261e65a98856cb7a77a05c085b7fb
Laurent Georget <laurent.georget@meteo-concept.fr>
2019-09-16 15:56:38 +0200

    Add a standalone executable to download from Weatherlink

---
+++ src/cassandra_utils.h
@@ -0,0 +1,45 @@
+/**
+ * @file cassandra_utils.h
+ * @brief Definition of some handy functions to manipulate Cassandra types
+ * @author Laurent Georget
+ * @date 2019-09-16
+ */
+/*
+ * Copyright (C) 2019  SAS Météo Concept <contact@meteo-concept.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ostream>
+
+#include <cassandra.h>
+
+bool operator==(const CassUuid& uuid1, const CassUuid& uuid2)
+{
+	return uuid1.time_and_version == uuid2.time_and_version;
+}
+
+bool operator<(const CassUuid& uuid1, const CassUuid& uuid2)
+{
+	return uuid1.time_and_version < uuid2.time_and_version || (uuid1.time_and_version == uuid2.time_and_version && uuid1.clock_seq_and_node < uuid2.clock_seq_and_node);
+}
+
+std::ostream& operator<<(std::ostream& os, const CassUuid& uuid)
+{
+	char str[CASS_UUID_STRING_LENGTH];
+	cass_uuid_string(uuid, str);
+	os << str;
+	return os;
+}
+
