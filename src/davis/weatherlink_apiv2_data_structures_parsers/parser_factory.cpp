ae8a7e41dd6a95463d1a8917d072c882066832b8
Laurent Georget <laurent.georget@meteo-concept.fr>
2024-05-15 18:08:13 +0200

    Extend the Davis Transmitter parser use to all ISSs emitting the same data structure

--- src/davis/weatherlink_apiv2_data_structures_parsers/parser_factory.cpp
+++ src/davis/weatherlink_apiv2_data_structures_parsers/parser_factory.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 
+#include "davis/abstract_weatherlink_api_message.h"
 #include "sentek_probe_116_parser.h"
 #include "soil_probe_108_parser.h"
 #include "thermohygro_probe_100_parser.h"
@@ -20,7 +21,9 @@ std::unique_ptr<AbstractParser> ParserFactory::makeParser(int sensorType, std::m
 		return std::make_unique<SoilProbe108Parser>(variables["soil_moisture"]);
 	} else if (sensorType == 115 || sensorType == 116) {
 		return std::make_unique<SentekProbe116Parser>();
-	} else if (sensorType == 55) {
+	} else if (dataStructureType == AbstractWeatherlinkApiMessage::DataStructureType::WEATHERLINK_CONSOLE_ISS_CURRENT_READING ||
+		   dataStructureType == AbstractWeatherlinkApiMessage::DataStructureType::WEATHERLINK_CONSOLE_ISS_ARCHIVE_RECORD  ||
+		   sensorType == 55) {
 		return std::make_unique<DavisTransmitter55Parser>(std::move(variables), dataStructureType);
 	} else {
 		return nullptr;
